e37c3ad0cf4deaa27fbb3019020956a5cfb06f42
Sébastien Celles <s.celles@gmail.com>
2021-10-11 09:20:55 +0200

    WIP

--- sia_vac_to_xcsoar.py
+++ sia_vac_to_xcsoar.py
@@ -18,7 +18,10 @@ def sia_vac_to_xcsoar():
     wpt_details_path.mkdir(parents=True, exist_ok=True)
 
     with open(wpt_details_path / "WPT_DETAILS.txt", "w") as fd:
-        for code in df.index:
+        for i, code in enumerate(df.index):
+            print(f"proceed {code} {i+1}/%d" % len(df.index))
+            # ToDo : copy vac pdf file
+
             print(f"[{code}]", file=fd)
             # print("COUNTRY: France", file=fd)
             # print("", file=fd)
@@ -31,11 +34,14 @@ def sia_vac_to_xcsoar():
             print(f"ICAO: {code}", file=fd)
             print("", file=fd)
             filename_vac = f"AD-2.{code}"
+            print(f"image=wpt_details/{filename_vac}" + ".pdf", file=fd)
             images = convert_from_path(vac_path / (filename_vac + ".pdf"))
+            (wpt_details_path / code).mkdir(parents=True, exist_ok=True)
             for i, image in enumerate(images):
-                image.save(wpt_details_path / f"page{i}.jpg", "JPEG")
-
-            print(f"image={filename_vac}", file=fd)
+                picture_filename = f"AD-2.{code}-%02d.jpg" % (i + 1)
+                page_path = wpt_details_path / code / picture_filename
+                print(f"image=wpt_details/{code}/{picture_filename}", file=fd)
+                image.save(page_path, "JPEG")
             print("", file=fd)
         
     # convert -density 150 presentation.pdf -quality 90 output-%3d.jpg
