45fd2d90bde4b702df6c2486724a2b0904e9f085
Sébastien Celles <scls19fr@users.noreply.github.com>
2021-10-18 21:04:30 +0200

    Update sia_vac_to_xcsoar.py

--- sia_vac_to_xcsoar.py
+++ sia_vac_to_xcsoar.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 from airac import airac_date
 from download_sia_vac import download_sia_vac
-from pdf2image import convert_from_path
+# from pdf2image import convert_from_path
 
 
 def sia_vac_to_xcsoar():
@@ -14,12 +14,13 @@ def sia_vac_to_xcsoar():
     df = df.set_index("code")
     print(df)
 
-    wpt_details_path = vac_path / "wpt_details"
-    wpt_details_path.mkdir(parents=True, exist_ok=True)
+    # wpt_details_path = vac_path / "wpt_details"
+    # wpt_details_path.mkdir(parents=True, exist_ok=True)
 
-    with open(wpt_details_path / "WPT_DETAILS.txt", "w") as fd:
+    #with open(wpt_details_path / "WPT_DETAILS.txt", "w") as fd:
+    with open(vac_path / "WPT_DETAILS.txt", "w") as fd:
         for i, code in enumerate(df.index):
-            print(f"proceed {code} {i+1}/%d" % len(df.index), end="")
+            print(f"proceed {code} {i+1}/%d" % len(df.index))
             # ToDo : copy vac pdf file
 
             print(f"[{code}]", file=fd)
@@ -32,18 +33,21 @@ def sia_vac_to_xcsoar():
             # print("REMARKS:", file=fd)
             # print("", file=fd)
             print(f"ICAO: {code}", file=fd)
-            print("", file=fd)
+            # print("", file=fd)
             filename_vac = f"AD-2.{code}"
-            print(f"image=wpt_details/{filename_vac}" + ".pdf", file=fd)
-            images = convert_from_path(vac_path / (filename_vac + ".pdf"))
-            (wpt_details_path / code).mkdir(parents=True, exist_ok=True)
-            pages = len(images)
-            print(f" with {pages} page(s)")
-            for j, image in enumerate(images):
-                picture_filename = f"AD-2.{code}-%02d.jpg" % (j + 1)
-                page_path = wpt_details_path / code / picture_filename
-                print(f"image=wpt_details/{code}/{picture_filename}", file=fd)
-                image.save(page_path, "JPEG")
+            print(f"file=wpt_details/{filename_vac}" + ".pdf", file=fd)
+            # print(f"file=wpt_details/{code}/{filename_vac}" + ".pdf", file=fd)
+            
+            # images = convert_from_path(vac_path / (filename_vac + ".pdf"))
+            # (wpt_details_path / code).mkdir(parents=True, exist_ok=True)
+            # pages = len(images)
+            # print(f" with {pages} page(s)")
+            # for j, image in enumerate(images):
+            #     picture_filename = f"AD-2.{code}-%02d.jpg" % (j + 1)
+            #     page_path = wpt_details_path / code / picture_filename
+            #     print(f"image=wpt_details/{code}/{picture_filename}", file=fd)
+            #     image.save(page_path, "JPEG")
+            
             print("", file=fd)
         
     # convert -density 150 presentation.pdf -quality 90 output-%3d.jpg
